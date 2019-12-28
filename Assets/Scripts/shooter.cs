fd4ea3ace9ce1d7b69c47e37648391444756bbb3
Manuel Velazquez <manueldejesusramiro.velazquez@bbva.com>
2019-12-28 14:05:29 -0600

    inicial

---
+++ Assets/Scripts/shooter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooter : MonoBehaviour
+{
+    public GameObject bullet;
+    public float firerate;
+    private float nextFire;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) && Time.time >nextFire)
+        {
+            nextFire = Time.time + firerate;
+            Instantiate(bullet, gameObject.transform.position, gameObject.transform.rotation);
+        }
+    }
+}
---
+++ Temp/gradleOut/launcher/build/generated/source/buildConfig/release/com/vetapia/prueba/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.vetapia.prueba;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.vetapia.prueba";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "0.1";
+}
